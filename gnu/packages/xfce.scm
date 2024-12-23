Bost/guix

commit 2e6adef5a61e074299c5331e04a43f1039b0921e

gnu: xfce4-power-manager: Update to 4.20.0.
* gnu/packages/xfce.scm (xfce4-power-manager): Update to 4.20.0.
[arguments]: Add 'patch-configure phase.
[native-inputs]: Add glib:bin.

Change-Id: I094a7ae49dab6f8a8f380d033766eccff2142807

gnu/packages/xfce.scm
@@ -1345,7 +1345,7 @@ system resources, while still being visually appealing and user friendly.")
 (define-public xfce4-power-manager
   (package
     (name "xfce4-power-manager")
-    (version "4.18.4")
+    (version "4.20.0")
     (source (origin
               (method url-fetch)
               (uri (string-append "https://archive.xfce.org/src/xfce/"
@@ -1353,10 +1353,19 @@ system resources, while still being visually appealing and user friendly.")
                                   "xfce4-power-manager-" version ".tar.bz2"))
               (sha256
                (base32
-                "0x6qychcgqxc5dwwxzypqw2da35y6cd25ngg42zxndnrvixqz4bn"))))
+                "0agdsq2d4kr9aw7nqj1x5cgpxqcjffajipwjvlxq6likyv7924wp"))))
     (build-system gnu-build-system)
+    (arguments
+     (list #:phases
+           #~(modify-phases %standard-phases
+               (add-before 'configure 'patch-configure
+                 (lambda _
+                   (substitute* "configure"
+                     ;; XDG_CHECK_PACKAGE_BINARY requires an absolute path.
+                     (("\\$PKG_CONFIG --variable=gdbus_codegen gio-2.0")
+                      "type -p gdbus-codegen")))))))
     (native-inputs
-     (list pkg-config intltool))
+     (list (list glib "bin") pkg-config intltool))
     (inputs
      (list libxrandr gtk+ upower libnotify libxfce4ui))
     (home-page "https://www.xfce.org/")

commit f014b78a6a3417aa65ba6f4656d50f9f71922b32

gnu: xfce4-power-manager: Fix guix system build caused by 2e6adef5a6.
* gnu/packages/xfce.scm (xfce4-power-manager): Fix guix system build problem
introduced by 2e6adef5a6. The `guix system build` fails with 'In procedure
opendir: No such file or directory' for
/gnu/store/70x83bfg3hqvsvhfrk5fw4m57wm6sb73-xfce4-power-manager-4.20.0.

Change-Id: Id18b3ed33c13ddf7669525bb0f516aed35ead117

gnu/packages/xfce.scm
@@ -1370,7 +1370,7 @@ system resources, while still being visually appealing and user friendly.")
     (native-inputs
      (list (list glib "bin") pkg-config intltool))
     (inputs
-     (list libxrandr gtk+ upower libnotify libxfce4ui))
+     (list libxrandr gtk+ upower libnotify libxfce4ui polkit))
     (home-page "https://www.xfce.org/")
     (synopsis "Xfce Power Manager")
     (description